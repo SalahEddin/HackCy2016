6b84e295ed63167ea2943f2c900c8164998ee0b1
salah eddin alshaal <salshaal@gmail.com>
2016-06-25 01:03:02 +0300

    get accelerometer

--- gymme/ViewController.swift
+++ gymme/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var band: MSBClient?
+    @IBOutlet weak var GalvanicLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up ClientManager and its delegate
+        MSBClientManager.sharedManager().delegate = self
+        
+        // Get a list of attached Clients
+        let attachedClients:Array = MSBClientManager.sharedManager().attachedClients()
+        
+        // Connect to the Band Client
+        if let client = attachedClients.first as? MSBClient {
+            self.band = client
+            MSBClientManager.sharedManager().connectClient(self.band)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +37,37 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : MSBClientManagerDelegate {
+    ////////////////////////////////
+    //MARK: Protocol Conformation
+    ////////////////
+    // MARK: Client Manager Delegates
+    internal func clientManager(clientManager: MSBClientManager!, clientDidConnect client: MSBClient!) {
+        // debug mesage
+        print("Band connected.")
+        // retreive the tile if it exists, or create a new tile
+        // getTile();
+        do{
+            try self.band?.sensorManager.startAccelerometerUpdatesToQueue(nil, withHandler:{
+                (accelerometerData: MSBSensorAccelerometerData!, error: NSError!) in
+                print(accelerometerData.x)
+                self.GalvanicLabel.text = String(accelerometerData.x)
+            })
+        }
+        catch{
+        }
+    }
+    
+    internal func clientManager(clientManager: MSBClientManager!, clientDidDisconnect client: MSBClient!) {
+        print("Band disconnected.")
+    }
+    
+    internal func clientManager(clientManager: MSBClientManager!, client: MSBClient!, didFailToConnectWithError error: NSError!) {
+        print("Failed to connect to Band.")
+        print(error.description)
+    }
+    
 }
 
---
+++ gymme/gymme-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef Band_Swift_Bridging_Header_h
+#define Band_Swift_Bridging_Header_h
+
+#import <MicrosoftBandKit_iOS/MicrosoftBandKit_iOS.h>
+
+#endif
